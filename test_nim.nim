514ccf9ecfec3203fb028d0a81188d2345ab7f8e
docik <docik@1.ru>
2023-08-09 16:15:21 +0300

    add command line argument

--- test_nim.nim
+++ test_nim.nim
@@ -1,10 +1,13 @@
 import times
+import os
+from std/strutils import Digits, parseInt
 
 proc fibonacci(n: int): int = 
     if n <= 1:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 
+var iterations = parseInt(paramStr(1))
 var starttime = cpuTime()
-echo(fibonacci(50))
+echo(fibonacci(iterations))
 echo int(cpuTime() - starttime), " sec"

04a0f340ce21ea7846c5c5afc7532ba2b7d9c375
docik <docik@1.ru>
2023-08-09 16:53:32 +0300

    add command line argument

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,16 @@
 #include <time.h>
+#include <printf.h>
+#include <stdlib.h>
 
-int fibonacci(int n) {
+unsigned long long fibonacci(unsigned long long int n) {
     if (n <= 1) {
         return n;
     } 
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
     time_t starttime=time(NULL);	
-    printf("%i\n", fibonacci(45));
+    printf("%llu\n", fibonacci(atoi(argv[1])));
     printf("%dsec\n",time(NULL)-starttime);
 }

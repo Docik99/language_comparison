8c4bc654a804c95de99f65ba2bd4b0230e76001a
docik <docik@1.ru>
2023-08-09 16:26:41 +0300

    add command line argument

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,15 @@
 import time
+import sys
+
 
 def fibonacci(n):
     if n <= 1:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
 
-starttime = time.time()
-print(fibonacci(45))
-print(int(time.time() - starttime), " sec")
+
+if __name__ == "__main__":
+    iterations = int(sys.argv[1])
+    starttime = time.time()
+    print(fibonacci(iterations))
+    print(int(time.time() - starttime), " sec")
